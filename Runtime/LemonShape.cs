a2c56fe9914e583c6ca061ffd836625a47b3f013
filipdufka-ryzen-pc <filipdufka@gmail.com>
2022-09-11 21:32:25 +0200

    Lemon Shape Start

--- Runtime/LemonShape.cs
+++ Runtime/LemonShape.cs
@@ -1,10 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace FruitBowl.Lemon {
+	enum ShapeAdditions
+    {
+		Union, Subtraction, Intersection 
+    }
+	
 	/// <summary>
-	/// Lemon Shape consist of multiple LemonPaths
+	/// Lemon Shape consist of multiple closed LemonPaths.
 	/// </summary>
-	public class LemonShape : MonoBehaviour {
-		//TODO
+	/// 
+
+	public class LemonShape {
+
+		LemonBounds bounds = new LemonBounds();
+		List<(LemonPath,ShapeAdditions)> pathList = new List<(LemonPath, ShapeAdditions)>();
+
+		public void AddPathUnion(LemonPath path) { 
+			pathList.Add((path,ShapeAdditions.Union));
+		}
+
 	}
 }
